3697b10f0c29bbf6bd5827d4808fe1b64c9f6c70
broy011 <36655629+broy011@users.noreply.github.com>
2018-07-12 00:43:19 +0530

    Add files via upload

---
+++ src/main/java/demo/StarterApplication.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class StarterApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(StarterApplication.class, args);
+	}
+
+
+}
+
+
+
+
+
