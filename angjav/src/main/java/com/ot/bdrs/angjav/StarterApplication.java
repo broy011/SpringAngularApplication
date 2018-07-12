5448fb6b65a9397ef305fa23feecd832f6b3535a
broy011 <broy@opentext.com>
2018-07-12 09:30:25 +0530

    Base Spring Boot App

---
+++ angjav/src/main/java/com/ot/bdrs/angjav/StarterApplication.java
@@ -0,0 +1,14 @@
+package com.ot.bdrs.angjav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterApplication 
+{
+    	public static void main(String[] args) throws Exception {
+    		SpringApplication.run(StarterApplication.class, args);
+    	}
+
+    }
+
